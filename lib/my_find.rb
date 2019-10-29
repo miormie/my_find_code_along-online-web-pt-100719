8e11a244424a3d1713d47d255189d255f51ce2b4
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-29 02:17:22 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+ return collection[i] if yield(collection[i])
+    
+    i = i + 1
+  end
 end
+
+
+
+# if yield(collection[i]) (line6)
+#     return collection[i] (line7)
+# end (line8)
